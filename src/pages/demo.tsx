6157fae4711cdb43ef5b93caea8da31be8ff948a
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-09-22 20:30:18 -0500

    Envolviendo las rutas en <Router>

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,1 +1,4 @@
-//
+const TestComponent = () => {
+  return( <div>Componente de prueba</div>);
+};
+export default TestComponent;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,13 @@
-//
+import { lazy } from 'react';
+const TestPrueba = lazy(() => import('../pages/demo'))
+
+const coreRoutes = [
+
+    {
+        path: '/test',
+        title: 'test',
+        component:TestPrueba ,
+    }
+]
+const routes = [...coreRoutes];
+export default routes;
