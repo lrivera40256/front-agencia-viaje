51fdd9805de606524e55da54b2498d6122c77e6a
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-09-26 19:13:50 -0500

    Se envia lista de roles y permisos para el formulario de creacion(Llena los select)

--- src/pages/rolePermission.tsx
+++ src/pages/rolePermission.tsx
@@ -2,26 +2,56 @@ import { useEffect, useState } from "react";
 import { deleteRolePermissionById, getRolePermission } from "../services/rolePermissionService"
 import type { RolePermission } from "../models/RolePermission";
 import Table from "../components/Table";
-import Form from "@/components/Form";
-import { log } from "console";
+import Form from "../components/Form";
+import { getPermissions } from "../services/permissionService";
+import { getRoles } from "@/services/roleService";
 
 const rolePermissionPage: React.FC = () => {
 const [rolesPermissions, setRolesPermissions] = useState<Object[]>([]);
+const [permissions, setPermissions] = useState<{value:string, label:string}[]>([]);
+const [roles, setRoles] = useState<{value:string, label:string}[]>([]);
 const [loading, setLoading] = useState(false);
 const [showForm, setShowForm] = useState(false);
+
 
     const loadData = async () => {
         const rolesPermissions = await getRolePermission();
 
         try {
             const data = rolesPermissions.map( rp => ({
-            ...rp,
-            role: rp.role.name,
-            permission: rp.permission.url,
+                ...rp,
+                role: rp.role.name,
+                permission: rp.permission.url,
             }));
             setRolesPermissions(data);
         } catch (error) {
             throw error;
+        }
+    };
+
+    const loadPermissions = async () => {
+        const permissions = await getPermissions();
+        try {
+            const data = permissions.map( p => ({
+                value: p._id,
+                label: `${p.method}: ${p.url}`
+            }));
+            setPermissions(data);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    const loadRoles = async () => {
+        const roles = await getRoles();
+        try {
+            const data = roles.map( r => ({
+                value: r._id,
+                label: r.name
+            }))
+            setRoles(data)
+        } catch (error) {
+            throw error
         }
     };
         
@@ -38,6 +68,14 @@ const [showForm, setShowForm] = useState(false);
     useEffect(() =>{
         loadData();
     }, []);
+
+    useEffect(() => {
+        if (showForm){
+            loadPermissions();
+            loadRoles();
+        }
+    },[showForm]);
+    
     return (
         <>
         <Table 
@@ -52,23 +90,34 @@ const [showForm, setShowForm] = useState(false);
                     variant:"danger"
                 }
             ]}
-            emptyMessage={loading ? "Cargando..." : "No hay roles"}
+            emptyMessage={loading ? "Cargando..." : "No hay role-permiso"}
             className="mt-4"
         />
-        {/* {showForm && ( 
+        {showForm && (
             <Form
             title="Crear rol - permiso"
             fields={[
-                { name: "roleId", label: "Permiso id" },
-                { name: "permissionId", label: "Rol id" },
+                { 
+                    name: "role", 
+                    label: "Permiso", 
+                    type: "select", 
+                    options:permissions
+                },
+                { 
+                    name: "permission", 
+                    label: "Rol", 
+                    type: "select",
+                    options:roles
+                },
             ]}
-            initialValues={{ roleId: "", permissuonId: "" }}
+            initialValues={{ roleId: "", permissionId: "" }}
             onSubmit={async (values) => {
-                console.log(values);
+                console.log(values.permissionId);
+                console.log(values.roleId);
             }}
             submitText="Crear"
             />
-        )} */}
+        )}
         </>
     )
 }
