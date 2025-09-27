dfc3ac01ef257bdb5acd6a88c9cce03423bfc099
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-09-26 23:37:13 -0500

    Se modifica role_permission para crear correctamente

--- src/pages/rolePermission.tsx
+++ src/pages/rolePermission.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { deleteRolePermissionById, getRolePermission } from "../services/rolePermissionService"
+import { createRolePermission, deleteRolePermissionById, getRolePermission } from "../services/rolePermissionService"
 import type { RolePermission } from "../models/RolePermission";
 import Table from "../components/Table";
 import Form from "../components/Form";
 import { getPermissions } from "../services/permissionService";
 import { getRoles } from "@/services/roleService";
+import { toast } from "sonner";
 
 const rolePermissionPage: React.FC = () => {
 const [rolesPermissions, setRolesPermissions] = useState<Object[]>([]);
@@ -64,6 +65,9 @@ const [showForm, setShowForm] = useState(false);
         setShowForm(true);
         console.log("Agregar rol - permiso");
     };
+    const closeForm = () => {
+        setShowForm(false);
+    };
        
     useEffect(() =>{
         loadData();
@@ -74,6 +78,7 @@ const [showForm, setShowForm] = useState(false);
             loadPermissions();
             loadRoles();
         }
+        loadData();
     },[showForm]);
     
     return (
@@ -94,29 +99,44 @@ const [showForm, setShowForm] = useState(false);
             className="mt-4"
         />
         {showForm && (
-            <Form
-            title="Crear rol - permiso"
-            fields={[
-                { 
-                    name: "role", 
-                    label: "Permiso", 
-                    type: "select", 
-                    options:permissions
-                },
-                { 
-                    name: "permission", 
-                    label: "Rol", 
-                    type: "select",
-                    options:roles
-                },
-            ]}
-            initialValues={{ roleId: "", permissionId: "" }}
-            onSubmit={async (values) => {
-                console.log(values.permissionId);
-                console.log(values.roleId);
-            }}
-            submitText="Crear"
-            />
+        <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+            <div className="bg-white rounded-xl shadow-lg p-3 w-full max-w-xs my-12">
+                <Form
+                    title="Crear rol - permiso"
+                    fields={[
+                        { 
+                            name: "permission", 
+                            label: "Permiso", 
+                            type: "select", 
+                            options:permissions
+                        },
+                        { 
+                            name: "role", 
+                            label: "Rol", 
+                            type: "select",
+                            options:roles
+                        },
+                    ]}
+                    initialValues={{ permission: "", role: "" }}
+                    onSubmit={async (values) => {
+                        try {
+                            await createRolePermission(values.role, values.permission);
+                            toast.success("Rol-permiso creado exitosamente");
+                        } catch (error) {
+                            toast.error("Error al crear rol-permiso");
+                        }
+                        setShowForm(false)
+                    }}
+                    submitText="Crear"
+                />
+                <button
+                    className="mt-1 w-full py-2 rounded bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold"
+                    onClick={closeForm}
+                >
+                    Cancelar
+                </button>
+            </div>
+        </div>
         )}
         </>
     )
