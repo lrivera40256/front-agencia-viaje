f577c9cb591386619bf4f9841c455c78ed4bd74a
lrivera40256 <luis.rivera40256@ucaldas.edu.co>
2025-09-29 20:58:02 -0500

    Se actualiza auth para que verifique si existe token en LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Suspense, useState } from 'react';
 import routes from './routes/index';
 import { DashboardLayout } from './components/layout/DashboardLayout';
 import { LoginForm } from './components/auth/LoginForm';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
 
 				{/* Rutas del dashboard */}
 				<Route element={<DashboardLayout />}>
+					<Route element={<ProtectedRoute />}>
 					{routes.map((route, index) => {
 						const { path, component: Component } = route;
 						return (
@@ -29,6 +31,7 @@ function App() {
 							/>
 						);
 					})}
+					</Route>
 				</Route>
 			</Routes>
 		</Router>
