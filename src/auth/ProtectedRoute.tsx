a4f3614b1fdd5ff7e9174102ec546a4d656555c8
Jacobo Arroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-10-04 11:20:38 -0500

    Se limpia el codigo de logs

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -3,15 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 // Función para verificar si el usuario está autenticado
 const isAuthenticated = () => {
     let token = localStorage.getItem("token");
-    console.log(token);
-    
     return token ? true : false;
 };
 
 // Componente de Ruta Protegida
-const ProtectedRoute = () => {
-    console.log(isAuthenticated());
-    
+const ProtectedRoute = () => {    
     return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
