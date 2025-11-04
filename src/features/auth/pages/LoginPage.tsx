0fc3d1f85651b627d34eaa466c54dd652539a36a
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-03 23:28:45 -0500

    refactor:Se independiza las paginas de auth

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "../components/LoginForm";
+
+export function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-50 to-blue-100">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/features/auth/pages/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "../components/RegisterForm";
+
+export function RegisterPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-50 to-blue-100">
+      <RegisterForm />
+    </div>
+  );
+}
