458ff2adca1ad4c34c32a638580d7a22a5a2f946
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-08 22:39:04 -0500

    feat:Se crean servicios de ciudades y departamentos

--- src/services/departamentService.ts
+++ src/services/departamentService.ts
@@ -1,10 +1,10 @@
-import api from "@/interceptors/msSecurityInterceptor";
+import api from "@/interceptors/msLogicInterceptor";
 import { Departament } from "@/models/departaments";
 
 export const DepartamentService = {
     getAllDepartaments: async (): Promise<Departament[]> => {
         try {
-            const { data } = await api.get("/api/departaments");
+            const { data } = await api.get("/departaments");
             return data;
         } catch (error) {
             console.error("Error fetching departaments:", error);
