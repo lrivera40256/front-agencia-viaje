4d9f13accb4f28acce392ecff47cbe9caf727b0a
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-08 22:46:00 -0500

    feat:Creacion y actualizacion de algunos tipos

---
+++ src/features/trip-form/types/segment.types.ts
@@ -0,0 +1,13 @@
+import { Vehicle } from "./vehicle.types";
+
+export interface Segment {
+  order: number;
+  dateFrom: Date;
+  dateTo: Date;
+  departamentFrom: number;
+  departamentTo: number;
+  cityFrom: number;
+  cityTo: number;
+  vehicle?: Vehicle;
+  hotel: number;
+}
