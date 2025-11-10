e7ea9a9712d7b2d8f2bf11d057d79e224a1d5019
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-10 13:01:54 -0500

    style:Se cambia el diseño por cada hotel

--- src/features/accommodation/components/HotelItem.tsx
+++ src/features/accommodation/components/HotelItem.tsx
@@ -3,51 +3,41 @@ import hotellmage from "@/assets/hotel.png";
 import { Hotel } from "../types/hotel.type";
 interface HotelItemProps {
     hotel: Hotel
-    selected: boolean;
     setSelected: (hotelId: number) => void;
 }
 
-export const HotelItem = ({ hotel, selected, setSelected }: HotelItemProps) => {
+export const HotelItem = ({ hotel, setSelected }: HotelItemProps) => {
 
-
-  return (
-    <div
-      onClick={() => setSelected(hotel.id)}
-      className={`
+    return (
+        <div
+            onClick={() => setSelected(hotel.id)}
+            className={`
         rounded-xl overflow-hidden cursor-pointer transition-all
         bg-white border shadow-md hover:shadow-lg
-        ${selected ? "border-blue-500 shadow-blue-300" : "border-gray-200"}
       `}
-    >
-      {/* Imagen */}
-      <div className="relative h-48 w-full">
-        <img
-          src={hotellmage}
-          alt="hotel"
-          className="object-cover w-full h-full"
-        />
+        >
+            {/* Imagen */}
+            <div className="relative h-48 w-full">
+                <img
+                    src={hotellmage}
+                    alt="hotel"
+                    className="object-cover w-full h-full"
+                />
+            </div>
 
-        {/* etiqueta seleccionado */}
-        {selected && (
-          <span className="absolute top-3 right-3 bg-blue-600 text-white text-xs px-3 py-1 rounded-full shadow-lg">
-            Seleccionado
-          </span>
-        )}
-      </div>
+            {/* Contenido */}
+            <div className="p-5 text-center space-y-2">
+                <h3 className="text-xl font-semibold text-gray-800">{hotel.name}</h3>
 
-      {/* Contenido */}
-      <div className="p-5 text-center space-y-2">
-        <h3 className="text-xl font-semibold text-gray-800">{hotel.name}</h3>
+                <p className="text-yellow-500 text-sm">
+                    {Array.from({ length: hotel.stars })
+                        .map(() => "★")
+                        .join(" ")}
+                </p>
 
-        <p className="text-yellow-500 text-sm">
-          {Array.from({ length: hotel.stars })
-            .map(() => "★")
-            .join(" ")}
-        </p>
-
-        <p className="text-gray-600 text-sm">{hotel.amenities}</p>
-      </div>
-    </div>
-  );
+                <p className="text-gray-600 text-sm">{hotel.amenities}</p>
+            </div>
+        </div>
+    );
 };
 
