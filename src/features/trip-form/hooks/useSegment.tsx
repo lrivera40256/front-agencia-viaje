a1cc75ca36069f3406a627d51e92c26a7a8466d1
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-10 13:21:19 -0500

    fix:correcion al crear un nuevo segmento

--- src/features/trip-form/hooks/useSegment.tsx
+++ src/features/trip-form/hooks/useSegment.tsx
@@ -7,21 +7,26 @@ export function useTripSegment() {
   const create = (order: number): Segment => {
     const newSeg: Segment = {
       order,
-      cityFrom: 0,
-      cityTo: 0,
-      departamentFrom:0,
-      departamentTo:0,
+      cityFrom: { id: 0, name: "" },
+      cityTo: { id: 0, name: "" },
+      departamentFrom: { id: 0, name: "" },
+      departamentTo: { id: 0, name: "" },
       dateFrom: new Date(),
       dateTo: new Date(),
-      hotel: 0,
+      hotel: {
+        id: 0,
+        name: "",
+        stars: 0,
+        amenities: ""
+      },
       rooms: [],
     };
     setSegment(newSeg);
     return newSeg;
   };
   const updateField = (field: keyof Segment, value: Segment[keyof Segment]) => {
-    console.log(field,value);
-    
+    console.log(field, value);
+
     setSegment(s => s ? { ...s, [field]: value } : undefined);
   }
 
