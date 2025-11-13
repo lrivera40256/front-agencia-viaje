79a15f6338919d9f3f88d0eb160d52797f28c433
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-13 00:54:33 -0500

    fix:Se corrige importacion no usada

--- src/features/profile/services/profileService.ts
+++ src/features/profile/services/profileService.ts
@@ -1,23 +1,24 @@
 // import type { UserProfile } from "../types/profile.types";
 
 import api from "@/interceptors/axiosInterceptor";
-import { ProfileDto } from "@/services/profileService";
 
 export async function getProfile() {
   const res = await api.get(`/profiles/user`);
+  console.log(res);
+  
   return res.data;
 }
 export async function patchProfile(
   profileId: string,
   payload: { phone?: string; photoId?: string; user?: { name?: string; email?: string } }
-): Promise<ProfileDto> {
+) {
   const { data } = await api.patch(`/profiles/${profileId}`, payload);
   return data;
 }
-export async function uploadProfilePhoto(profileId: string, file: File): Promise<ProfileDto> {
+export async function uploadProfilePhoto(profileId: string, file: File) {
   const fd = new FormData();
   fd.append('file', file);
   const { data } = await api.patch(`/profiles/${profileId}/photo/upload`, fd);
-  return data as ProfileDto;
+  return data;
 }
 
