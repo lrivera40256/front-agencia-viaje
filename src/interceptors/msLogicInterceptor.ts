0e79da2e5e033f8f9ba26c7ffd6add99adf5f52c
JacoboArroyave <jacobo.arroyave46095@ucaldas.edu.co>
2025-11-17 21:30:49 -0500

    fix:Se decomenta la navegacion al home

--- src/interceptors/msLogicInterceptor.ts
+++ src/interceptors/msLogicInterceptor.ts
@@ -11,7 +11,7 @@ api.interceptors.response.use(
     const status = error?.response?.status;
 
     if (status === 403) {
-      // window.location.href = '/';
+      window.location.href = '/';
     }
   }
 );
